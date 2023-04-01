d832669b9c6c9a5c370b912eaa7819329da501e5
matej-sveda <mates.sveda@gmail.com>
2023-04-01 15:23:17 +0200

    write tests for second task (hreflangs)
    - check redirect form non-supported language
    - check all supported hreflangs (extract all supported hreflangs to json filr in fixtures)

---
+++ cypress/e2e/druhyUkol.cy.js
@@ -0,0 +1,26 @@
+import expectedHreflangs from '../fixtures/hreflangs.json'
+
+describe.only('druha uloha', () => {
+  const initialUrl = 'https://www.kiwi.com/mx/cheap-flights/london-united-kingdom/istanbul-turkey/'
+  beforeEach(() => {
+    cy.visit(initialUrl)
+  })
+
+  it('Checks the language from URL is not supported', () => {
+    cy.log('Check if the URL has been redirected')
+    cy.url().should('not.eq', initialUrl)
+    cy.log('Check if the language of the page has changed')
+    cy.document().then(doc => {
+      const lang = doc.documentElement.getAttribute('lang')
+      expect(lang).to.not.equal('mx')
+      expect(lang).to.equal('es')
+    })
+  })
+  it.only('Checks the hreflangs', () => {
+    cy.log('Compare extracted hreflangs with actual hreflangs')
+    cy.get('link[rel="alternate"]').then($hreflangs => {
+      const hreflangs = Array.from($hreflangs).map(link => link.getAttribute('hreflang'))
+      expect(hreflangs).to.have.members(expectedHreflangs)
+    })
+  })
+})
