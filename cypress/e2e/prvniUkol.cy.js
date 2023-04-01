b18c71b621637ee7cf7ff172a95c96df557f61f6
jozef.sefcik <jozef.sefcik@eset.com>
2023-04-01 15:12:25 +0200

    verify URL responses

--- cypress/e2e/prvniUkol.cy.js
+++ cypress/e2e/prvniUkol.cy.js
@@ -1,6 +1,6 @@
-describe('template spec', () => {
+describe('prvni ukol', () => {
   beforeEach(() => {
-    cy.visit('https://www.kiwi.com/en/country/china/?botview=1')
+    cy.visit('/country/china/?botview=1')
   })
 
   it('Checks title, meta description, and canonical URL', () => {
@@ -20,5 +20,53 @@ describe('template spec', () => {
       'href',
       'https://www.kiwi.com/en/country/china/',
     )
+  })
+  it('should verify the response of each airline URL', () => {
+    const airlines = [
+      { name: 'Ruili Airlines', url: 'https://www.kiwi.com/en/airline/dr/ruili-airlines/' },
+      {
+        name: 'China Eastern Airlines',
+        url: 'https://www.kiwi.com/en/airline/mu/china-eastern-airlines/',
+      },
+      { name: 'Sichuan Airlines', url: 'https://www.kiwi.com/en/airline/3u/sichuan-airlines/' },
+      { name: 'Lucky air', url: 'https://www.kiwi.com/en/airline/8l/lucky-air/' },
+      { name: 'Urumqi Airlines', url: 'https://www.kiwi.com/en/airline/uq/urumqi-airlines/' },
+      { name: 'China Airlines', url: 'https://www.kiwi.com/en/airline/ci/china-airlines/' },
+      {
+        name: 'China Express Airlines',
+        url: 'https://www.kiwi.com/en/airline/g5/china-express-airlines/',
+      },
+      { name: 'Hainan Airlines', url: 'https://www.kiwi.com/en/airline/hu/hainan-airlines/' },
+      { name: 'Loong Air', url: 'https://www.kiwi.com/en/airline/gj/loong-air/' },
+      { name: 'China United', url: 'https://www.kiwi.com/en/airline/kn/china-united/' },
+      {
+        name: 'Genghis Khan Airlines',
+        url: 'https://www.kiwi.com/en/airline/9d/genghis-khan-airlines/',
+      },
+      { name: 'Air Travel', url: 'https://www.kiwi.com/en/airline/a6/air-travel/' },
+      {
+        name: 'Colorful Guizhou Airlines',
+        url: 'https://www.kiwi.com/en/airline/gy/colorful-guizhou-airlines/',
+      },
+      { name: 'Qingdao Airlines', url: 'https://www.kiwi.com/en/airline/qw/qingdao-airlines/' },
+      { name: 'Grand China Air', url: 'https://www.kiwi.com/en/airline/cn/grand-china-air/' },
+      { name: 'Shanghai Airlines', url: 'https://www.kiwi.com/en/airline/fm/shanghai-airlines/' },
+      {
+        name: 'China Southern Airlines',
+        url: 'https://www.kiwi.com/en/airline/cz/china-southern-airlines/',
+      },
+      { name: 'Air Changan', url: 'https://www.kiwi.com/en/airline/9h/air-changan/' },
+      { name: 'Tijan Airlines', url: 'https://www.kiwi.com/en/airline/gs/tianjin-airlines/' },
+      { name: '9 Air', url: 'https://www.kiwi.com/en/airline/aq/9-air/' },
+    ]
+
+    cy.visit('/country/china/?botview=1')
+    airlines.forEach(airline => {
+      cy.get('[class*=TextLink__StyledTextLink]').contains(airline.name).click()
+      cy.request('GET', airline.url).then(response => {
+        expect(response.status).to.eq(200) // or 302
+      })
+      cy.go('back')
+    })
   })
 })
